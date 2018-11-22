1ee96e012de230e7443b74fc671719b33357e1f2
Timur Grebnev <thrillofground@gmail.com>
2018-11-22 22:15:43 +0300

    update 2

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
+var modalState = {
+    modal: null,
+    lastInvokedId: null
+}
+
 $.when($.ready).then(function() {
     makeSchedule()
     initTabs()
 });
 
-const dateFormatter = new Intl.DateTimeFormat("ru-RU", {
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric"
+const dateFormatter = new Intl.DateTimeFormat('ru-RU', {
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
 })
 
-const dateFormatterLong = new Intl.DateTimeFormat("ru-RU", {
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric"
+const dateFormatterLong = new Intl.DateTimeFormat('ru-RU', {
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    weekday: 'long',
+    year: 'numeric'
 })
 
 const addresses = {
@@ -67,33 +74,34 @@ function dateForApi() {
 }
 
 function createScheduleBlock(apiItems) {
-    console.log(apiItems)
+    console.log(apiItems) // убрать
 
     for (const item of apiItems) {
         const data = extractMasterData(item)
         var masterCard = $(makeMasterCard(data))
-        masterCard.data(item.id, item)
+        masterCard.data('item', item)
         $('.mc-cards').append(masterCard)
     }
 
-    initListeners()
+    initListeners().then(function() {
+        modalState.modal = $('.enroll-modal').clone(true)
+    })
 }
 
 function createErrorBlock() {
     errorBlock = $(`<div class="mc-error">
-                        Нам очень жаль, но что-то пошло не так
-                        и мы не смогли загрузить расписание для этой страницы
+                        Нам очень жаль, но что-то пошло не так,
+                        и мы не смогли загрузить расписани
                     </div>`)
     $('.master-classes').append(errorBlock)
 }
 
 function extractMasterData(apiItem) {
     const cover = extractCover(apiItem)
+    const title = extractTitle(apiItem)
+    var age = extractAge(apiItem)
 
     const lesson = apiItem.baseLesson
-    const title = extractTitle(lesson)
-
-    var age = extractAge(lesson)
 
     var theme
     if (lesson.minClass === null && lesson.minAge === null) {
@@ -113,32 +121,35 @@ function extractMasterData(apiItem) {
     const address = extractAddress(apiItem)
 
     return {
-        theme,
-        cover,
         day: date.day,
         month: date.month,
         time: date.time,
+        theme,
+        cover,
         title,
         age,
         address
     }
 }
 
-function extractAge(lessonInfo) {
+function extractAge(apiItem) {
+    const lessonInfo = apiItem.baseLesson
     var age
 
     if (!(lessonInfo.minClass || lessonInfo.minAge)) {
         age = ''
     } else if (lessonInfo.minClass) {
-        age = `(${lessonInfo.minClass}-${lessonInfo.maxClass} кл.)`
+        age = `${lessonInfo.minClass}-${lessonInfo.maxClass} кл.`
     } else {
-        age = `(${lessonInfo.minAge}-${lessonInfo.maxAge} лет)`
+        age = `${lessonInfo.minAge}-${lessonInfo.maxAge} лет`
     }
 
     return age
 }
 
-function extractTitle(lessonInfo) {
+function extractTitle(apiItem) {
+    const lessonInfo = apiItem.baseLesson
+
     return lessonInfo.name.trim()
         .split(' ')
         .slice(1, -1)
@@ -146,24 +157,24 @@ function extractTitle(lessonInfo) {
 }
 
 function extractCover(apiItem) {
-    const defaultCover = 'https://storage.geekclass.ru/images/080d1774-fece-462b-9388-46cc311eb657.jpg'
+    const defaultCover = 'img/cover-default.jpg'
     const cover = apiItem.cover ? apiItem.cover : defaultCover
 
     return cover
 }
 
 function formatDate(format, date) {
-    date = format.formatToParts(new Date(date))
+    const dateParts = format.formatToParts(new Date(date))
+    var dateObject = {}
 
-    const day = date[0].value
-    const month = date[2].value
-    const time = date[4].value + ':' + date[6].value
-
-    return {
-        day,
-        month,
-        time
+    for (const part of dateParts) {
+        if (part.type != 'literal') {
+            dateObject[part.type] = part.value
+        }
     }
+    dateObject.time = dateObject.hour + ':' + dateObject.minute
+
+    return dateObject
 }
 
 function extractAddress(apiItem) {
@@ -199,26 +210,77 @@ function makeMasterCard(masterData) {
             </div>
         </div>
     </div>
-    ` // more button отдельно
+    `
 
     return template
 }
 
 function initListeners() {
-    $('.modal-close').click(function() {
-        $(this).parent().parent().removeClass('open')
-    })
+    return new Promise(function(resolve) {
+        console.log('promising')
 
-    $('.more').click(function() {
-        $('.enroll-modal-box').addClass('open')
+        $('.modal-close').click(function() {
+            $(this).parent().parent().removeClass('open')
+        })
+
+        $('.more').click(function() {
+            const data = $(this).parent().data('item')
+
+            if (data.id != modalState.lastInvokedId) {
+                $('.enroll-modal').replaceWith(modalState.modal)
+            }
+
+            fillModal(data)
+            $('.enroll-modal-box').addClass('open')
+        })
+
+        $('.show-form').click(function() {
+            $(this).parent().slideUp()
+            $('.modal-enroll').slideDown()
+        })
+
+        $('.modal-enroll-form').submit(function(e) { // доделать
+            e.preventDefault();
+
+            $.ajax({
+                type: 'POST',
+                url: $('form').attr('action'),
+                data: $('form').serialize(),
+                //or your custom data either as object {foo: 'bar', ...} or foo=bar&...
+                success: function(response) {},
+            });
+
+            console.log('submitted');
+        });
+
+        resolve('initiated')
     })
 }
 
 function fillModal(apiItem) {
-    title
-    date
-    duration
-    age
-    address
-    id
+    const address = extractAddress(apiItem)
+    const id = apiItem.id
+    const date = formatDate(dateFormatterLong, apiItem.startDate)
+    const cover = extractCover(apiItem)
+    const title = extractTitle(apiItem)
+    const age = extractAge(apiItem)
+    const duration = apiItem.baseLesson.duration
+    const fullDate = `${date.day} ${date.month} ${date.year}, ${date.weekday}`
+
+    $('.modal-header-cover').attr('src', cover)
+    $('.modal-header-title').text(title)
+    if (age) {
+        $('.header-extra-item.age span').text(age)
+    } else {
+        $('.header-extra-item.age').hide()
+    }
+    if (duration) {
+        $('.header-extra-item.duration span').text(duration + ' мин')
+    } else {
+        $('.header-extra-item.duration').hide()
+    }
+    $('.modal-enroll-form input[name="lessonId"]').val(id)
+    $('.event-info-item.date').text(fullDate)
+    $('.event-info-item.address').text(address)
+    $('.event-info-item.time').text(date.time)
 }
